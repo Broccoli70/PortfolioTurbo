ecf6ab04ca8de8b74c3c5dfee09bf84df010c02f
lukasz biernacki <thelubb1@gmail.com>
2021-05-31 17:25:34 +0200

    Add files via upload

---
+++ animation_mobile.js
@@ -0,0 +1,212 @@
+$(function(){
+
+
+    // RESPONSIVE
+    const mobile = window.matchMedia("(max-width: 576px)");
+  
+    function handleTabletChange(e) {
+      if (e.matches) {
+        console.log('Apka na fony!')
+        animation();
+      }
+    }
+    mobile.addListener(handleTabletChange)
+    handleTabletChange(mobile)
+
+    // RESPONSIVE-END
+    
+    function animation() {
+
+    lax.init()
+  
+    $(window).on('scroll', ()=> {
+  
+      lax.addDriver('scrollY', function () {
+        return window.scrollY
+      });
+  
+      lax.addElements('.logo', {
+        scrollY: {
+          opacity: [
+            [0, 'elInY + (screenHeight*0.75)'],
+            [0, 1]
+          ],
+          translateX: [
+            ['elInY', 100],
+            [-200, 0]
+          ],
+          translateY: [
+            ['elInY', 'elOutY'],
+            [0, 100]
+          ]
+        }
+  
+      })
+  
+      // SCROLL DOWN
+      lax.addElements('#start', {
+        scrollY: {
+          opacity: [
+            [0, 150],
+            [1, 0]
+          ],
+          scale: [
+            [0, 400],
+            [1, 4]
+          ],
+        }
+  
+      })
+  
+      // LUKASZ BIERNACKI
+      lax.addElements('.name', {
+        scrollY: {
+          opacity: [
+            [0, 'elInY + (screenHeight*0.75)'],
+            [0, 1]
+          ],
+          translateY: [
+            ['elInY', 'elOutY'],
+            [0, 100]
+          ]
+        }
+  
+      })
+  
+  
+      // PORTFOLIO 2021
+      lax.addElements('#flow', {
+        scrollY: {
+          opacity: [
+            [0, 'elInY + (screenHeight*0.75)'],
+            [0, 1]
+          ],
+          translateX: [
+            ['elInY', 'elOutY-200'],
+            [100, 0]
+          ],
+          translateY: [
+            ['elInY', 'elOutY'],
+            [0, 100]
+          ]
+        }
+      })
+  
+      //Glowa 3d
+      lax.addElements('.header_picture', {
+        scrollY: {
+          opacity: [
+            ['screenHeight/6', 'elOutY/3'],
+            [0, 1]
+          ],
+          translateY: [
+            ['elInY', 'elOutY'],
+            [100, -600]
+          ],
+        }
+      })
+  
+      //KIM JESTEM
+      lax.addElements('#headline_1', {
+        scrollY: {
+          opacity: [
+            ['elInY + (screenHeight/4)' , 'elInY + (screenHeight/2)'],
+            [0, 1]
+          ],
+          // translateY: [
+          //   ['elInY', 'elOutY'],
+          //   [0, 100]
+          // ]
+        }
+      })
+  
+      //TEXT 1
+      lax.addElements('.text', {
+        scrollY: {
+          opacity: [
+            ['elInY + (screenHeight/4)' , 'elInY + (screenHeight/2)'],
+            [0, 1]
+          ],
+          // translateY: [
+          //   ['elInY', 'elOutY'],
+          //   [0, 100]
+          // ]
+        }
+      })
+  
+      //BUTTON
+      lax.addElements('.actual_work', {
+        scrollY: {
+          opacity: [
+            ['elInY + (screenHeight/4)', 'elInY + (screenHeight/3)'],
+            [0, 1]
+          ],
+          // translateY: [
+          //   ['elInY', 'elOutY'],
+          //   [0, 100]
+          // ],
+          scale: [
+            ['elInY', 'elInY + (screenHeight/2)'],
+            [1.15, 1]
+          ]
+        }
+      })
+  
+      //HEADLINE 2
+      lax.addElements('#headline_2', {
+        scrollY: {
+          opacity: [
+            ['elInY + (screenHeight/4)' , 'elInY + (screenHeight/2)'],
+            [0, 1]
+          ],
+          // translateY: [
+          //   ['elInY', 'elOutY'],
+          //   [0, 100]
+          // ]
+        }
+      })
+  
+      //TEXT 2
+      lax.addElements('#text_2', {
+        scrollY: {
+          opacity: [
+            ['elInY + (screenHeight/4)' , 'elInY + (screenHeight/2)'],
+            [0, 1]
+          ],
+          // translateY: [
+          //   ['elInY', 'elOutY'],
+          //   [0, 100]
+          // ]
+        }
+      })
+  
+      
+      //IMG 1
+      lax.addElements('#img', {
+        scrollY: {
+          opacity: [
+            ['elInY + (screenHeight/2)' , 'elInY + (screenHeight*0.7)'],
+            [0, 1]
+          ],
+          // translateY: [
+          //   ['elInY', 'elOutY'],
+          //   [0, 100]
+          // ]
+        }
+      })
+  
+      //Grafika
+      lax.addElements('#grafika', {
+        scrollY: {
+          opacity: [
+            ['elInY + (screenHeight/5)', 'elInY + (screenHeight/2)'],
+            [0, 1]
+          ],
+        }
+      })
+  
+    
+    })
+    }
+  })
+  
